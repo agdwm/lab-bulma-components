27a6c2cfe09e40328529d9a7dee1db13d2185792
agdwm <almudenaguerraswm@gmail.com>
2018-11-30 17:06:37 +0100

    Created FormField Component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {NavBar} from "./navBar";
+import {FormField} from "./formField";
 
 export const App = () => {
 	return (
 		<div className="container">
 			<NavBar/>
+			<main className="main-content">
+				<FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+				<FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+			</main>
 		</div>
 	)
 }
